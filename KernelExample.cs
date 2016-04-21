84239bdb0b13a1a3ad5a0daf32b94f91c1fedf3e
yamagamirenya <sk8.dance.eureka@gmail.com>
2016-04-22 01:03:14 +0900

    Added files via upload

---
+++ KernelExample.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KernelExample : MonoBehaviour {
+
+    public ComputeShader shader;
+
+	// Use this for initialization
+	void Start () {
+        ComputeBuffer buffer = new ComputeBuffer(4,sizeof(int));
+        shader.SetBuffer(0, "buffer1", buffer);
+        shader.Dispatch(0, 1, 1, 1);
+        int[] data = new int[4];
+        buffer.GetData(data);
+        for(int i = 0; i < 4; i++)
+        {
+
+
+            buffer.Release();
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
