ec89646057a3dc224b9554a627332ebc2181e00f
yamagamirenya <sk8.dance.eureka@gmail.com>
2016-04-22 18:26:27 +0900

    Added files via upload

---
+++ Assets/Collision.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public partial class Collision : MonoBehaviour {
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "ob_cube")
+        {
+            Behaviour b = (Behaviour)collision.gameObject.GetComponent("Halo");
+            b.enabled = true;
+        }
+    }
+
+    void OnCollisionExit(Collision collision)
+    {
+        Rigidbody rigidbody = GetComponent<Rigidbody>();
+        if (collision.gameObject.tag == "ob_cube")
+        {
+            Behaviour b = (Behaviour)collision.gameObject.GetComponent("Halo");
+            b.enabled = false;
+            Vector3 v = rigidbody.velocity;
+            if (v.magnitude < 15)
+            {
+                v *= 2.0f;
+                if (v.magnitude < 5)
+                {
+                    v *= 2.0f;
+                }
+                rigidbody.velocity = v;
+            }
+        }
+        if (collision.gameObject.tag == "ob_wall")
+        {
+            Vector3 v = rigidbody.velocity;
+            if (v.magnitude < 15)
+            {
+                v *= 2.0f;
+                if (v.magnitude < 5)
+                {
+                    v *= 2.0f;
+                }
+                rigidbody.velocity = v;
+            }
+        }
+    }
+}
---
+++ Assets/test.shader
@@ -0,0 +1,29 @@
+﻿Shader "Custom/test Texture" {
+	SubShader{
+		Pass{
+		CGPROGRAM
+
+	#pragma vertex vert
+	#pragma fragment frag
+	#pragma target 3.0
+
+		struct appdata_base {
+		float4 vertex : POSITION;
+		float3 normal : NORMAL;
+		float4 texcood: TEXCOORD0;
+		};
+
+	float4 vert(appdata_base v) :POSITION{
+		return mul(UNITY_MATRIX_MVP,v.vertex);
+	}
+
+	fixed4 frag(float4 sp : WPOS) : COLOR{
+		fixed2 red_green = sp.xy / _ScreenParams.xy;
+	fixed blue = 0.0;
+	fixed alpha = 0.5f;
+	return fixed4(red_green, blue, alpha);
+	}
+	ENDCG
+		}
+	}
+}
